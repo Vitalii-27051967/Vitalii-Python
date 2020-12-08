cf0f2c5a94e88964596da0f574c4bc8fa1b722e0
Vitalii-27051967 <74895661+Vitalii-27051967@users.noreply.github.com>
2020-12-08 08:02:33 +0200

    Add files via upload
    Исправлено

--- code 8.py
+++ code 8.py
@@ -33,17 +33,39 @@ def read_file(name_file):
     return data
 
 
-clear_domains = []            # создание списка доменов
-file_1 = read_file(file_domains)
-[clear_domains.append(file_1[dig].replace(".", "")) for dig in range(len(file_1))]      # Убираем точки
-names_1 = read_file(file_names)            # создание списка c имёнами
-names_2 = []
-[names_2.append(names_1[dig].split("\t")) for dig in range(len(names_1))]  # разбивка по разделителю
-clear_name = []                             # создание списка имён
-[clear_name.append(number[1]) for number in names_2]
-rand_int = random.randint(100, 999)         # случайные числа от 100 до 999
-domains = clear_domains[random.randint(0, len(clear_domains)-1)]  # случайные домены из clear_domains
-names = clear_name[random.randint(0, len(clear_name)-1)]  # случайные имена из clear_name
-letters = "".join(random.choice(string.ascii_lowercase) for x in range(random.randint(5, 7)))   # случайные буквы
-rand_mail = f"{names}.{rand_int}@{letters}.{domains}"       # переменная, если с ней нужно дальше работать
-print("Сгенерированный адрес", rand_mail, "\n", "Тип данных", type(rand_mail))
+# 1. Написать функцию, которая считывает из файла domains.txt
+def write_domains(r_file, target):      # создание и обработка списка доменов
+    clear_d = []
+    file_1 = r_file(target)
+    [clear_d.append(file_1[dig].replace(".", "")) for dig in range(len(file_1))]      # Убираем точки
+    return clear_d
+
+
+# 2. Написать функцию, которая считывает данные из файла names.txt
+def write_name(r_file, target):
+    names_1 = r_file(target)
+    names_2 = []
+    [names_2.append(names_1[dig].split("\t")) for dig in range(len(names_1))]   # разбивка по разделителю
+    clear_n = []                                                                # создание списка имён
+    [clear_n.append(number[1]) for number in names_2]
+    return clear_n
+
+
+# 3. Написать функцию для генерирования e-mail
+def rnd_email(cl_domains, cl_name):
+    rand_int = random.randint(100, 999)         # случайные числа от 100 до 999
+    domains = cl_domains[random.randint(0, len(cl_domains) - 1)]  # случайные домены из cl_domains
+    names = cl_name[random.randint(0, len(cl_name) - 1)]  # случайные имена из clear_name
+    letters = "".join(random.choice(string.ascii_lowercase) for _ in range(random.randint(5, 7)))   # случайные буквы
+    r_mail = f"{names}.{rand_int}@{letters}.{domains}"       # переменная, если с ней нужно дальше работать
+    return r_mail
+
+
+clear_domains = write_domains(read_file, file_domains)
+print("Проверка доменов", clear_domains)
+
+clear_name = write_name(read_file, file_names)
+print("Проверка имён", clear_name, "\n\n")
+
+rand_mail = rnd_email(clear_domains, clear_name)
+print("Сгенерированный адрес", rand_mail, "\n", "Тип данных адреса", type(rand_mail))
