ee56d9c2cf4e8802233e8f31a716c87be9465a9b
Vitalii-27051967 <74895661+Vitalii-27051967@users.noreply.github.com>
2020-12-27 11:03:47 +0200

    Add files via upload

--- code 12_2.py
+++ code 12_2.py
@@ -15,83 +15,99 @@
 
 import os
 import re
-import calendar
 import json
+import calendar
 
 my_dir = "c:/Files for Me"                      # моя рабочая директория
-check_file = os.path.exists(my_dir)             # проверяем наличие папки по заданному пути.
+check_file = os.path.exists(my_dir)             # проверяю наличие папки по заданному пути.
 print("Проверка наличия папки -->", check_file)
 if check_file is False:                         # если папка не существувет, то создаём её.
     os.mkdir("c:/Files for Me")
 os.chdir(my_dir)
-file_name = "authors.txt"                       # Имя файла как по умолчанию для чтения
-file_data_json = "authors_json.json"            # Название файла для записи данных JSON
 
+file_name = "authors.txt"                       # Имя файла по умолчанию для чтения
+file_data_json = "authors_json.json"            # Название файла для записи данных JSON
 #  _______________________________________________________________________________________________________________
 #   2.1) написать функцию, которая считывает данные из этого файла "authors.txt"
 
-with open(file_name, "r", encoding='utf-8') as file:
-    data = []
-    for line in file.readlines():
-        data.append(line.strip())                       # Строка данных считанных из файла
-print("\t\n", "Считанные данные из файла: ", end="\n\n")
-[print(dat) for dat in data]
-print(end="\n\n")
 
-
+def get_list_authors(filename):
+    with open(filename, "r", encoding='utf-8') as file:
+        data_f = []
+        for line in file.readlines():
+            data_f.append(line.strip())        # Строка данных считанных из файла
+    return data_f
 #  _______________________________________________________________________________________________________________
 # 2.2) Написать функцию, которая принимает список строк полученной в пункте 2.1, и возвращает список словарей
 
-def sep(n):                 # Делает разбивку текста на дату и автора
-    d_bd = n.split("-")             # отделяем дату от текста
-    d_bd_1 = d_bd[0]
-    d_bd = d_bd[1].split("'s")      # отделяем автора из текста
-    d_bd_2 = d_bd[0]
-    return d_bd_1, d_bd_2
 
+def get_dict_authors(data):
+    def sep(str_txt):                         # Делает разбивку текста на дату и автора
+        d_bd = str_txt.split("-")             # отделяем дату от текста
+        data_begin_end = d_bd[0]        # дата
+        d_bd = d_bd[1].split("'s")      # отделяем автора из текста
+        author_from_text = d_bd[0]      # автор
+        return data_begin_end, author_from_text
 
-def dig_month(n):           # Преобразует дату с текстом к числовому формату dd/mm/yyyy
-    part_2_d = ""
-    part = n[0].split()                                         # Разбиваем дату на составляющие
-    part_1 = "".join(re.findall(r'\d+', part[0])).zfill(2)      # Число месяца, приводим к формату DD /mm/yyyy
-    part_2 = "".join(re.findall(r'\D+', part[1]))               # Месяц в текстовом формате
-    part_3 = "".join(re.findall(r'\d+', part[2]))       # Год
-    for k, v in enumerate(calendar.month_name):         # Делаем замену текстовых названий мес. на цифровые
-        if part_2 == v:
-            part_2_d = str(k).zfill(2)                  # Приводим к формату dd/ MM /yyyy
-    dat_d = f"{part_1}/{part_2_d}/{part_3}"
-    return dat_d
+    def dig_month(dat_txt):             # Преобразует дату с текстом к числовому формату dd/mm/yyyy
+        part_2_d = ""
+        part = dat_txt[0].split()                               # Разбиваем дату на составляющие
+        part_1 = "".join(re.findall(r'\d+', part[0])).zfill(2)  # Число месяца приводим к формату DD /mm/yyyy
+        part_2 = "".join(re.findall(r'\D+', part[1]))           # Месяц в текстовом формате
+        part_3 = "".join(re.findall(r'\d+', part[2]))           # Год
+        for month_digit, month_text in enumerate(calendar.month_name):   # Делаем замену TXT названий мес. на DIG
+            if part_2 == month_text:
+                part_2_d = str(month_digit).zfill(2)                     # Приводим месяц к формату dd/ MM /yyyy
+        dat_d = f"{part_1}/{part_2_d}/{part_3}"
+        return dat_d
 
+    data_be = []
+    begin = dict()
+    dict_begin_full = []
 
-data_begin = []
-begin = dict()
-dict_begin_full = []
+    for num in data:
+        if re.search(r"\bbirthday\b", num.lower()) and re.search(r"'s", num.lower()) \
+                or re.search(r"\bdeath\b", num.lower()):
+            data_be.append(sep(num))
+            begin["name"] = sep(num)[1].lstrip()        # запись в словарь автора
+            begin["date"] = dig_month(sep(num))         # запись в словарь нормализованной даты
+            sss = begin.copy()
+            dict_begin_full.append(sss)
 
-# Создание словарей, с датами по условию наличия в тексте "birthday" или "death".
-# В условии нет данных на разбивку на 2 отдельных списка, или добавления дополнительного ключа в словарь, так что
-# пишем всё в один список dict_begin_full в формате {"name": name, "date": date}
-
-for num in data:
-    if re.search(r"\bbirthday\b", num.lower()) and re.search(r"'s", num.lower())\
-            or re.search(r"\bdeath\b", num.lower()):
-        data_begin.append(sep(num))
-        date_in_dig = dig_month(sep(num))
-        begin["name"] = sep(num)[1].lstrip()
-        begin["date"] = dig_month(sep(num))
-        sss = begin.copy()
-        dict_begin_full.append(sss)
-
-print("\n\t", "Проверка обработки данных:", end="\n\n")
-[print(num_1["name"].ljust(25, " "), num_1["date"]) for num_1 in dict_begin_full]
-
-
+    return dict_begin_full
 #  ---------------------------------------------------------------------------------------------------------------
 # 2.3) Написать функцию, которая сохраняет результат пункта 2.2 в json файл.
 
-with open(file_data_json, "w") as file:        # запись данных в файл
-    json.dump(dict_begin_full, file)
 
-with open(file_data_json, "r") as file:        # Чтение данных из файла для проверки
-    for_check = json.load(file)
+def write_json(file_dat, dict_beg):
+    with open(file_dat, "w") as file:
+        json.dump(dict_beg, file)
+    return
+#  ---------------------------------------------------------------------------------------------------------------
+# Чтение данных из файла для проверки
+
+
+def reader_file(file_json):
+    with open(file_json, "r") as file:
+        for_check = json.load(file)
+    return for_check
+#  ---------------------------------------------------------------------------------------------------------------
+# пошаговое выполнение программы
+
+
+authors_list = get_list_authors(file_name)      # чтение данных TXT из файла
+authors_dict = get_dict_authors(authors_list)   # создание списка словарей
+write_json(file_data_json, authors_dict)        # запись данных в файл "authors_json.json"
+check_record = reader_file(file_data_json)      # проверка записи в файл
+
+#  ---------------------------------------------------------------------------------------------------------------
+# Проверка работы
+
+print("\t\n", "Считанные данные из файла: ", end="\n\n")
+[print(dat) for dat in authors_list]
+
+print("\n\t", "Проверка обработки данных 'name....date':", end="\n\n")
+[print(num_1["name"].ljust(25, "."), num_1["date"]) for num_1 in authors_dict]
+
 print("\n\t", "Проверка записи в файл:", end="\n\n")
-[print(value) for value in for_check]
+[print(value) for value in check_record]
